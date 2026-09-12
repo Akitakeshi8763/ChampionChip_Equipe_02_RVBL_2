e722673948b596f81ce878d837ababbf87cf4bba
kai64327174-pixel <kai64327174@gmail.com>
2026-09-12 21:42:45 +0800

    Update Datapath_modules.v
    Update definition of ALU Operation in RISCV ALU Block to match FSM encoding

--- src/rtl/Datapath_modules.v
+++ src/rtl/Datapath_modules.v
@@ -288,17 +288,17 @@ endmodule
 
 //  ---------- INLCUDED BLOCK: RISCV_ALU_4bit  ---------- 
 /* ALU Operations */
-`define c_ALU_OP_ADD    4'b0000
-`define c_ALU_OP_SUB    4'b0001
-`define c_ALU_OP_AND    4'b0010
-`define c_ALU_OP_OR     4'b0011
-`define c_ALU_OP_XOR    4'b0100
-`define c_ALU_OP_SLL    4'b0101
-`define c_ALU_OP_SRL    4'b0110
-`define c_ALU_OP_SRA    4'b0111
-`define c_ALU_OP_SLT    4'b1000
-`define c_ALU_OP_SLTU   4'b1001
-`define c_ALU_OP_PASS_B 4'b1010
+`define c_ALU_OP_PASS_B 4'h0
+`define c_ALU_OP_ADD    4'h1
+`define c_ALU_OP_SUB    4'h2
+`define c_ALU_OP_AND    4'h3
+`define c_ALU_OP_OR     4'h4
+`define c_ALU_OP_XOR    4'h5
+`define c_ALU_OP_SLL    4'h6
+`define c_ALU_OP_SRL    4'h7
+`define c_ALU_OP_SRA    4'h8
+`define c_ALU_OP_SLT    4'h9
+`define c_ALU_OP_SLTU   4'hA
 
 module RISCV_ALU_4bit (
     input [31:0] i_Register_Rs_1,
